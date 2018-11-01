0802eacfb9330ea8baab53e7746f85aa30d02f7c
sato-15 <sato.kazuhiko@gmail.com>
2018-11-01 22:52:22 +0900

    Add sources generated by Spring Initializr

---
+++ src/main/kotlin/jp/gr/java_conf/satok/objectifykotlinsample/ObjectifyKotlinSampleApplication.kt
@@ -0,0 +1,11 @@
+package jp.gr.java_conf.satok.objectifykotlinsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ObjectifyKotlinSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ObjectifyKotlinSampleApplication>(*args)
+}
---
+++ src/test/kotlin/jp/gr/java_conf/satok/objectifykotlinsample/ObjectifyKotlinSampleApplicationTests.kt
@@ -0,0 +1,16 @@
+package jp.gr.java_conf.satok.objectifykotlinsample
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ObjectifyKotlinSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

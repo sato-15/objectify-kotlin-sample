1df24db2d0a8c5c5c07c4d1cfcf9eeac1e00e57b
sato-15 <sato.kazuhiko@gmail.com>
2018-11-10 18:16:16 +0900

    Refactor to split into some methods

--- src/main/kotlin/jp/gr/java_conf/satok/objectify/sample/tool/ClassDiagramGenerator.kt
+++ src/main/kotlin/jp/gr/java_conf/satok/objectify/sample/tool/ClassDiagramGenerator.kt
@@ -19,63 +19,43 @@ class ClassDiagramGenerator {
         const val ENTITY_PACKAGE_PATH = "src/main/kotlin/jp/gr/java_conf/satok/objectify/sample/entity/"
         private const val SINGLE_LINE_DOC_REGEX: String = "/\\*\\* (.+) \\*/"
         private const val PARAMETER_REGEX: String = "(var|var)\\s+([a-zA-Z0-9_]+)\\s*:"
+        val dateFormat = SimpleDateFormat("yyyyMMdd-HHmmss")
     }
 
     var classMetaList = mutableListOf<ClassMeta>()
 
     fun generateUmlFile() {
         val files = Files.list(Paths.get(ENTITY_PACKAGE_PATH))
-
-        val df = SimpleDateFormat("yyyyMMdd-HHmmss")
-
         files.forEach {
-            val kClass = Class.forName("$ENTITY_PACKAGE.${it.fileName.toString().split(".")[0]}").kotlin
-            var entityClassName: String? = null
-            kClass.annotations.filter { entityClassName == null }.forEach {
-                if (it.annotationClass.simpleName == "Entity") {
-                    entityClassName = kClass.simpleName
-                }
-            }
-
-            var classMeta = ClassMeta(entityClassName!!, mutableListOf(), mutableMapOf())
-            classMetaList.add(classMeta)
-
-            kClass.memberProperties.forEach {
-                val simpleClassName = "${it.returnType.toString().removePrefix("kotlin.").removePrefix("$OBJECTIFY_PACKAGE.").replace("$ENTITY_PACKAGE.", "")}"
-                var propertyMeta = PropertyMeta(it.name, simpleClassName, mutableListOf(), 0, null)
-                classMeta.propertyMetaMap[it.name] = propertyMeta
-                it.javaField?.declaredAnnotations?.forEach {
-                    propertyMeta.annotationNames.add(it.annotationClass.simpleName!!)
-                }
-            }
-            generatePropertyMetaMap(it, classMeta)
+            generateClassMetaList(it!!)
         }
-
-        val classDiagramFile = File("class_diagram_${df.format(Date())}.pu").absoluteFile
-        this.classMetaList.forEach {
-            logger.debug("class ${it.name} {")
-            classDiagramFile.appendText("class ${it.name} {\n")
-            it.propertyMetaMap.toList().sortedBy { it.second.order }.forEach {
-                if(it.second.document != null) {
-                    logger.debug("    /'${it.second.document}'/")
-                    classDiagramFile.appendText("    /'${it.second.document}'/\n")
-                }
-                if(it.second.annotationNames.isNotEmpty()){
-                    it.second.annotationNames.forEach {
-                        logger.debug("    /'@$it'/")
-                        classDiagramFile.appendText("    /'@$it'/\n")
-                    }
-                }
-                logger.debug("    ${it.first}: ${it.second.typedClassName}")
-                classDiagramFile.appendText("    ${it.first}: ${it.second.typedClassName}\n")
-            }
-            logger.debug("}")
-            classDiagramFile.appendText("}\n")
-            classDiagramFile.appendText("\n")
-        }
+        writeUmlFile()
     }
 
-    fun generatePropertyMetaMap(path: Path, classMeta: ClassMeta): MutableMap<String, PropertyMeta> {
+    private fun generateClassMetaList(it: Path) {
+        val kClass = Class.forName("$ENTITY_PACKAGE.${it.fileName.toString().split(".")[0]}").kotlin
+        var entityClassName: String? = null
+        kClass.annotations.filter { entityClassName == null }.forEach {
+            if (it.annotationClass.simpleName == "Entity") {
+                entityClassName = kClass.simpleName
+            }
+        }
+
+        var classMeta = ClassMeta(entityClassName!!, mutableListOf(), mutableMapOf())
+        classMetaList.add(classMeta)
+
+        kClass.memberProperties.forEach {
+            val simpleClassName = "${it.returnType.toString().removePrefix("kotlin.").removePrefix("$OBJECTIFY_PACKAGE.").replace("$ENTITY_PACKAGE.", "")}"
+            var propertyMeta = PropertyMeta(it.name, simpleClassName, mutableListOf(), 0, null)
+            classMeta.propertyMetaMap[it.name] = propertyMeta
+            it.javaField?.declaredAnnotations?.forEach {
+                propertyMeta.annotationNames.add(it.annotationClass.simpleName!!)
+            }
+        }
+        generatePropertyMetaMap(it, classMeta)
+    }
+
+    private fun generatePropertyMetaMap(path: Path, classMeta: ClassMeta): MutableMap<String, PropertyMeta> {
         var propertyMetaMap = mutableMapOf<String, PropertyMeta>()
         var desc: String? = null
         var order = 0
@@ -96,5 +76,30 @@ class ClassDiagramGenerator {
             }
         }
         return propertyMetaMap
+    }
+
+    private fun writeUmlFile() {
+        val classDiagramFile = File("class_diagram_${dateFormat.format(Date())}.pu").absoluteFile
+        this.classMetaList.forEach {
+            logger.debug("class ${it.name} {")
+            classDiagramFile.appendText("class ${it.name} {\n")
+            it.propertyMetaMap.toList().sortedBy { it.second.order }.forEach {
+                if (it.second.document != null) {
+                    logger.debug("    /'${it.second.document}'/")
+                    classDiagramFile.appendText("    /'${it.second.document}'/\n")
+                }
+                if (it.second.annotationNames.isNotEmpty()) {
+                    it.second.annotationNames.forEach {
+                        logger.debug("    /'@$it'/")
+                        classDiagramFile.appendText("    /'@$it'/\n")
+                    }
+                }
+                logger.debug("    ${it.first}: ${it.second.typedClassName}")
+                classDiagramFile.appendText("    ${it.first}: ${it.second.typedClassName}\n")
+            }
+            logger.debug("}")
+            classDiagramFile.appendText("}\n")
+            classDiagramFile.appendText("\n")
+        }
     }
 }
